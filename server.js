8f2fb1d28af41993cd929f7400a0ad8078828b39
gloirekiba <gloirekiba21@gmail.com>
2022-07-03 22:28:27 +0200

    ✨ add joke route & refactor code

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ if (process.env.NODE_ENV !== "production") {
 
 const path = require("path");
 const express = require("express");
+const bodyParser = require("body-parser");
 const expressEjsLayout = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
+const jokeRouter = require("./routes/joke");
 
-mongoose.connect(process.env.DATABASE_URL);
+// Setup
+
 const app = express();
 
 app
@@ -17,14 +20,20 @@ app
   .set("view engine", "ejs")
   .set("layout", "layout/layout")
   .use(expressEjsLayout)
+  .use(bodyParser.urlencoded({ limit: "10mb", extended: false }))
   .use(express.static(path.resolve(__dirname, "public")));
 
+// Routing
+
+app.use("/", indexRouter);
+app.use("/joke", jokeRouter);
+
+// Database
+mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 
 db.on("error", (err) => console.log(err));
 db.once("open", () => console.log("connected to mongodb"));
-
-app.use("/", indexRouter);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port 3000`);
