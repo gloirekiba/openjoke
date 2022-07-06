52f3d0dcf02805e1f274f6ca001462eba755ef87
gloirekiba <gloirekiba21@gmail.com>
2022-07-06 12:02:23 +0200

    🚧 update register route

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,37 @@
 const express = require("express");
 
+const Contributor = require("../models/contributor");
+
 const router = express.Router();
 
-router.route("/").get((req, res) => {
-  res.render("register");
-});
+router
+  .route("/")
+  .get((req, res) => {
+    res.render("register");
+  })
+  .post(async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+      await new Contributor({
+        username,
+        password,
+      }).save();
+      res.render("register", {
+        alert: {
+          type: "success",
+          message: "Account created sucessfully",
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      res.render("register", {
+        alert: {
+          type: "danger",
+          message: "Something went wrong",
+        },
+      });
+    }
+  });
 
 module.exports = router;
