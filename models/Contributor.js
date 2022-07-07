5460f7785cef384e15a613d3228d2d8bd5e9c211
gloirekiba <gloirekiba21@gmail.com>
2022-07-07 21:46:35 +0200

    ✨ update contributor schema

--- models/Contributor.js
+++ models/Contributor.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const contributorSchema = new mongoose.Schema({
   username: {
     type: String,
+    lowercase: true,
     required: true,
   },
   password: {
